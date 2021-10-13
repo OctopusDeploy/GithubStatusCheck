53132bfc719c664fff26c43d3758694580379304
Corey <corey.underdown@octopus.com>
2021-10-13 14:36:40 +1000

    Azure Account variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,18 @@ variable "environment" {
   default = "Development"
 }
 
+variable "tenant_id" {}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
 provider "azurerm" {
   features {}
+
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 // Use the shared app service plan
---
+++ terraform/octopus.auto.tfvars
@@ -0,0 +1,7 @@
+environment = "#{Environment}"
+
+# Azure Service Principal
+tenant_id        = "#{AzureAccount.TenantId}"
+subscription_id  = "#{AzureAccount.SubscriptionNumber}"
+client_id        = "#{AzureAccount.Client}"
+client_secret    = "#{AzureAccount.Password}"
