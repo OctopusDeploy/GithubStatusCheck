4fecef227294ac28d8d7bf7866fa8ae772336d49
Andrew Best <andrew.best@octopus.com>
2022-06-17 13:50:51 +0930

    Adds documentation status check

---
+++ source/GitHubStatusChecksWebApp/Rules/DocumentationChainStatusRuleChecks.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotNet.Globbing;
+using Octokit;
+
+namespace GitHubStatusChecksWebApp.Rules;
+
+public class DocumentationChainStatusRuleChecks : IStatusCheck
+{
+    public string GetContext() => "Chain: Documentation";
+        
+    public bool MatchesRules(IEnumerable<PullRequestFile> files)
+    {
+        var glob = Glob.Parse("**/*.md");
+
+        return files.All(file => glob.IsMatch(file.FileName));
+    }
+}
